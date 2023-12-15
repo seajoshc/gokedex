dfb0c833cde776c6d718b7785630d2d071e87aa1
Josh Campbell <josh@p4a.net>
2023-12-14 22:52:45 -0800

    gofmt

--- internal/pokeapi/getLocationAreas.go
+++ internal/pokeapi/getLocationAreas.go
@@ -1,6 +1,7 @@
 package pokeapi
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -24,46 +25,46 @@ func GetLocationAreas() {
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
-    response, err := json.Marshal(payload)
-    if err != nil {
-        return err
-    }
-    w.Header().Set("Content-Type", "application/json")
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.WriteHeader(code)
-    w.Write(response)
-    return nil
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) error {
-    return respondWithJSON(w, code, map[string]string{"error": msg})
+	return respondWithJSON(w, code, map[string]string{"error": msg})
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    defer r.Body.Close()
-    type requestBody struct {
-        Email    string `json:"email"`
-        Password string `json:"password"`
-    }
-    type responseBody struct {
-        Token string `json:"token"`
-    }
+	defer r.Body.Close()
+	type requestBody struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	type responseBody struct {
+		Token string `json:"token"`
+	}
 
-    dat, err := io.ReadAll(r.Body)
-    if err != nil {
-        respondWithError(w, 500, "couldn't read request")
-        return
-    }
-    params := requestBody{}
-    err = json.Unmarshal(dat, &params)
-    if err != nil {
-        respondWithError(w, 500, "couldn't unmarshal parameters")
-        return
-    }
+	dat, err := io.ReadAll(r.Body)
+	if err != nil {
+		respondWithError(w, 500, "couldn't read request")
+		return
+	}
+	params := requestBody{}
+	err = json.Unmarshal(dat, &params)
+	if err != nil {
+		respondWithError(w, 500, "couldn't unmarshal parameters")
+		return
+	}
 
-    // do stuff with username and password
+	// do stuff with username and password
 
-    respondWithJSON(w, 200, responseBody{
-        Token: "example-auth-token",
-    })
+	respondWithJSON(w, 200, responseBody{
+		Token: "example-auth-token",
+	})
 }
