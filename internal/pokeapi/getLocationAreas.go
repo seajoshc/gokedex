776b77413b8fc802f75753b82f03021bf5683a4b
Josh Campbell <josh@p4a.net>
2023-12-14 22:51:21 -0800

    WIP adds boilerplate for working with json

--- internal/pokeapi/getLocationAreas.go
+++ internal/pokeapi/getLocationAreas.go
@@ -22,3 +22,48 @@ func GetLocationAreas() {
 	}
 	fmt.Printf("%s", body)
 }
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+    response, err := json.Marshal(payload)
+    if err != nil {
+        return err
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.WriteHeader(code)
+    w.Write(response)
+    return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+    return respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    defer r.Body.Close()
+    type requestBody struct {
+        Email    string `json:"email"`
+        Password string `json:"password"`
+    }
+    type responseBody struct {
+        Token string `json:"token"`
+    }
+
+    dat, err := io.ReadAll(r.Body)
+    if err != nil {
+        respondWithError(w, 500, "couldn't read request")
+        return
+    }
+    params := requestBody{}
+    err = json.Unmarshal(dat, &params)
+    if err != nil {
+        respondWithError(w, 500, "couldn't unmarshal parameters")
+        return
+    }
+
+    // do stuff with username and password
+
+    respondWithJSON(w, 200, responseBody{
+        Token: "example-auth-token",
+    })
+}
